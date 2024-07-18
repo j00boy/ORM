09242159856b365574f1520db4eec5d76e05dea7
junbeom-Son <junbeom247@gmail.com>
2024-07-18 09:48:30 +0900

    feat : create project

---
+++ orm-backend/src/main/java/orm/orm_backend/OrmBackendApplication.java
@@ -0,0 +1,13 @@
+package orm.orm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmBackendApplication.class, args);
+	}
+
+}
