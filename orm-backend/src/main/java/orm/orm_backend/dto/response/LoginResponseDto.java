5aa9eebd8be1bf0e8dd0942ef85f6098c3e1d826
junbeom-Son <junbeom247@gmail.com>
2024-07-19 12:44:49 +0900

    faet : complete LoginResponseDto

--- orm-backend/src/main/java/orm/orm_backend/dto/response/LoginResponseDto.java
+++ orm-backend/src/main/java/orm/orm_backend/dto/response/LoginResponseDto.java
@@ -1,2 +1,13 @@
-package orm.orm_backend.dto.response;public class LoginResponseDto {
+package orm.orm_backend.dto.response;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class LoginResponseDto {
+
+    private final String accessToken;
+    private final String imgSrc;
+    private final String nickname;
 }
