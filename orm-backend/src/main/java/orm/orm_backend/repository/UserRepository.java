22532e2fb6327f372f6b3754bafd2a3b96f64f53
junbeom-Son <junbeom247@gmail.com>
2024-07-19 13:39:32 +0900

    feat : check if the user joined before

--- orm-backend/src/main/java/orm/orm_backend/repository/UserRepository.java
+++ orm-backend/src/main/java/orm/orm_backend/repository/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import orm.orm_backend.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByKakaoId(Long kakaoId);
 }
--- orm-backend/src/main/java/orm/orm_backend/service/UserService.java
+++ orm-backend/src/main/java/orm/orm_backend/service/UserService.java
@@ -4,16 +4,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import orm.orm_backend.dto.response.LoginResponseDto;
+import orm.orm_backend.entity.User;
+import orm.orm_backend.entity.UserStatus;
+import orm.orm_backend.repository.UserRepository;
 import orm.orm_backend.util.KakaoUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final KakaoUtil kakaoUtil;
+    private final UserRepository userRepository;
     private final String ACCESS_TOKEN = "access_token";
     private final String REFRESH_TOKEN = "refresh_token";
 
@@ -26,7 +31,18 @@ public class UserService {
         result.put(REFRESH_TOKEN, refreshToken);
 
         LoginResponseDto userInfo = kakaoUtil.getKakaoUserInfo(accessToken);
+        if (!isJoined(userInfo.getKakaoId())) {
 
+        }
         return result;
+    }
+
+    private boolean isJoined(Long kakaoId) {
+        // 데이터베이스에 없는 사용자 -> 가입한적 없는 사용자
+        Optional<User> user = userRepository.findByKakaoId(kakaoId);
+        if (user.isEmpty()) {
+            return false;
+        }
+        return user.get().getIsActive() == UserStatus.Y;
     }
 }
