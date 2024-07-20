12f458dc78e7c2d4d362b21a22fda0fa6143e2a1
백승헌 <eric121@naver.com>
2024-07-20 20:16:07 +0900

    hotfix : restore project name

--- orm_android/app/src/main/java/com/orm/data/local/dao/MemberDao.kt
+++
@@ -1,9 +0,0 @@
-package com.orm.data.local.dao
-
-import androidx.room.Dao
-
-@Dao
-interface MemberDao {
-//    suspend fun getClubMembers(): List<Member>
-
-}
--- orm_android/app/src/main/java/com/orm/data/model/requestMember.kt
+++
@@ -1,7 +0,0 @@
-package com.orm.data.model
-
-data class requestMember(
-    val clubId: Int,
-    val introduction: String,
-    val userId: Int
-)
