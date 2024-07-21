c5cd5196d673824fe60d5c8f1787dde3d5c1b99f
junbeom-Son <junbeom247@gmail.com>
2024-07-21 23:46:10 +0900

    test : test automatic login with service access token

--- orm-backend/src/test/java/orm/orm_backend/controller/UserControllerTest.java
+++ orm-backend/src/test/java/orm/orm_backend/controller/UserControllerTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package orm.orm_backend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import orm.orm_backend.util.JwtUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
-  
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    int userId;
+    String accessToken;
+
+    @BeforeEach
+    void init() {
+        userId = 2;
+        accessToken = jwtUtil.createAccessToken(userId);
+    }
+
+    @Test
+    void autoLoginTest() throws Exception {
+        String url = "/users/login/auto";
+        mvc.perform(get(url).header("Authorization", accessToken))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 }
