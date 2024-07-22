3520b96a4d82cfd194fcbbb455a2e1edfcf658be
junbeom-Son <junbeom247@gmail.com>
2024-07-22 12:51:03 +0900

    feat : create trace creation request dto

---
+++ orm-backend/src/main/java/orm/orm_backend/dto/request/TraceCreationRequestDto.java
@@ -0,0 +1,11 @@
+package orm.orm_backend.dto.request;
+
+import java.sql.Date;
+
+public class TraceCreationRequestDto {
+
+    String title;
+    Integer mountainId;
+    Date hikingDate;
+    int routeId;
+}
