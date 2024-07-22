5e00ddeaca4806f5779418c91eb2f811dbdd41cd
junbeom-Son <junbeom247@gmail.com>
2024-07-22 10:58:52 +0900

    feat : create trace entity

---
+++ orm-backend/src/main/java/orm/orm_backend/entity/Trace.java
@@ -0,0 +1,36 @@
+package orm.orm_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Trace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn
+    private Trail trail;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @Column(length = 30)
+    private String title;
+
+    private Date hikingDate;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Float maxAltitude;
+    private String Course;
+}
