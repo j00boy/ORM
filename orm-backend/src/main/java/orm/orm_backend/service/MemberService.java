9c89913915eb56710e22aba0d6af752dd3992776
이찬진 <cj8291@naver.com>
2024-07-23 09:09:17 +0900

    feat: add method to find member in club at MemberService

--- orm-backend/src/main/java/orm/orm_backend/service/MemberService.java
+++ orm-backend/src/main/java/orm/orm_backend/service/MemberService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import orm.orm_backend.dto.request.MemberRequestDto;
 import orm.orm_backend.entity.Member;
 import orm.orm_backend.repository.MemberRepository;
 
@@ -27,7 +28,18 @@ public class MemberService {
         return result;
     }
 
+    // UserId에 해당하는 Page 형태의 Member 반환
     public Page<Member> getPageableMembers(Pageable pageable, Integer userId) {
         return memberRepository.findByUserId(pageable, userId);
+    }
+
+    // club에 속한 member 반환
+    public List<Member> getMembersInClub(Integer clubId) {
+        return memberRepository.findByClubId(clubId).stream().toList();
+    }
+
+    // Member 저장
+    public void saveMember(MemberRequestDto memberRequestDto) {
+        memberRepository.save(memberRequestDto.toEntity());
     }
 }
