6baa1703555651c23aa4f9fe65fe80a7d75101f4
백승헌 <eric121@naver.com>
2024-07-23 15:07:36 +0900

    fix : handle paging

--- ORM_Android/app/src/main/java/com/orm/ui/LauncherActivity.kt
+++ ORM_Android/app/src/main/java/com/orm/ui/LauncherActivity.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.orm.databinding.ActivityLauncherBinding
 import com.orm.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LauncherActivity : AppCompatActivity() {
@@ -26,7 +28,9 @@ class LauncherActivity : AppCompatActivity() {
             if (!token.isNullOrEmpty()) {
                 Log.d("LauncherActivity", "checkAccessToken: true, token: $token")
                 userViewModel.loginAuto()
-                startActivity(Intent(this, MainActivity::class.java))
+                startActivity(Intent(this, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                })
             } else {
                 Log.d("LauncherActivity", "checkAccessToken: false")
                 startActivity(Intent(this, LoginActivity::class.java))
