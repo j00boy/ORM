478590ec90033e2d36d1f359238bece85efeb46d
junbeom-Son <junbeom247@gmail.com>
2024-07-24 16:23:02 +0900

    feat : create image controller

---
+++ orm-backend/src/main/java/orm/orm_backend/controller/ImageController.java
@@ -0,0 +1,11 @@
+package orm.orm_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/images")
+public class ImageController {
+}
