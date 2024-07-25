4a36f18690531684dd6c756ad07897e57926159d
junbeom-Son <junbeom247@gmail.com>
2024-07-25 10:50:47 +0900

    chore : add firebase libraries

--- orm-backend/build.gradle.kts
+++ orm-backend/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("com.google.firebase:firebase-admin:9.3.0")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
