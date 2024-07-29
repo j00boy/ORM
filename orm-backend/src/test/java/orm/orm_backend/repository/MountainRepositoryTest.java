f23237edd840b3f2c36ebfc190c7f8ec975fc604
kkjjhh06 <kkjjhh06@naver.com>
2024-07-29 13:43:17 +0900

    test: test MountainRepository

--- orm-backend/src/test/java/orm/orm_backend/repository/MountainRepositoryTest.java
+++ orm-backend/src/test/java/orm/orm_backend/repository/MountainRepositoryTest.java
@@ -1,5 +1,6 @@
 package orm.orm_backend.repository;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,23 +8,91 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import orm.orm_backend.configuration.Mountain100Config;
+import orm.orm_backend.entity.Mountain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
 @DataJpaTest
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Slf4j
 public class MountainRepositoryTest {
 
     @Autowired
     private MountainRepository mountainRepository;
 
-    @BeforeEach
+    private List<Mountain> list = new ArrayList<>();
+    private Mountain mountain1;
+    private Mountain mountain2;
+    private Mountain mountain3;
+    private List<String> famous;
 
+    @BeforeEach
+    void setUp() {
+        mountain1 = Mountain.builder()
+                .mountainName("테스트 산1")
+                .mountainCode("11111111")
+                .address("테스트도 테스트시 테스트동")
+                .imageSrc("images/mountain1.jpg")
+                .description("test1")
+                .build();
+
+        mountain2 = Mountain.builder()
+                .mountainName("테스트 산2")
+                .mountainCode("22222222")
+                .address("테스트도 테스트시 테스트동")
+                .imageSrc("images/mountain2.jpg")
+                .description("test2")
+                .build();
+
+        mountain3 = Mountain.builder()
+                .mountainName("test 3")
+                .mountainCode("33333333")
+                .address("test test test")
+                .imageSrc("images/mountain3.jpg")
+                .description("test3")
+                .build();
+
+        list.add(mountain1);
+        list.add(mountain2);
+        list.add(mountain3);
+
+        mountainRepository.saveAll(list);
+    }
 
     @Test
-    @DisplayName("검색으로 산 가져오기")
-    public void getMountainByName() throws Exception {
+    @DisplayName("이름 검색으로 산 가져오기")
+    void getMountainByName() throws Exception {
+        // When
+        List<Mountain> mountains = mountainRepository.findByMountainNameContaining("테스트");
 
+        // Then
+        assertThat(mountains).hasSize(2);
+        assertThat(mountains).contains(mountain1, mountain2);
+        assertThat(mountains).doesNotContain(mountain3);
 
+        mountains.forEach(mountain -> log.info(mountain.getMountainName()));
+    }
+
+    @Test
+    @DisplayName("100대 명산 가져오기")
+    void get100Mountains() throws Exception {
+
+        // given
+        famous = Mountain100Config.CODE_100;
+
+        // When
+        List<Mountain> famousMountains = mountainRepository.findByMountainCodeIn(famous);
+
+        // Then
+        assertThat(famousMountains).hasSize(18);
+        assertThat(famousMountains).doesNotContain(mountain1);
+
+        famousMountains.forEach(mountain -> log.info(mountain.getMountainName()));
     }
  
 }
