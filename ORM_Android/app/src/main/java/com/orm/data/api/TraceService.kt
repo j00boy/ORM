3d0d69588f803dd373d85ef6f4a840dd41fdbc65
김세진 <991118ksj@naver.com>
2024-07-29 10:36:28 +0900

    feat : add token interceptor and add createTrace repository

--- ORM_Android/app/src/main/java/com/orm/data/api/TraceService.kt
+++ ORM_Android/app/src/main/java/com/orm/data/api/TraceService.kt
@@ -20,15 +20,12 @@ interface TraceService {
     // 발자국 생성
     @POST("trace/create")
     fun createTrace(
-        @Header("Authorization") accessToken: String,
         @Body trace: Trace
     ): Call<Trace>
 
     // 발자국 수정 (측정 전)
     @PATCH("trace/update")
-    @Headers("Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MjE3MDM4NDAsImV4cCI6MjA4MTcwMzg0MCwidXNlcklkIjo1fQ.KaAVNkL9-6pxSexWPrF6BOG8gL_HlKkF_JLzF2qnTiI")
     fun updateTrace(
-//        @Header("Authorization") accessToken: String,
         @Body trace: Trace
     ): Call<Unit>
 
@@ -36,24 +33,19 @@ interface TraceService {
     @Multipart
     @PATCH("trace/update/images/{traceId}")
     fun updateImages(
-        @Header("Authorization") accessToken: String,
         @Path("traceId") traceId: Int,
         @Part images: List<MultipartBody.Part>
     ): Call<Unit>
 
     // 발자국 측정 완료
     @PATCH("trace/measure-complete")
-    @Headers("Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MjE3MDM4NDAsImV4cCI6MjA4MTcwMzg0MCwidXNlcklkIjo1fQ.KaAVNkL9-6pxSexWPrF6BOG8gL_HlKkF_JLzF2qnTiI")
     fun measureComplete(
-//        @Header("Authorization") accessToken: String,
         @Body trace: Trace
     ): Call<Unit>
 
     // 발자국 삭제
     @DELETE("trace/{traceId}")
-    @Headers("Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MjE3MDM4NDAsImV4cCI6MjA4MTcwMzg0MCwidXNlcklkIjo1fQ.KaAVNkL9-6pxSexWPrF6BOG8gL_HlKkF_JLzF2qnTiI")
     fun deleteTrace(
-//        @Header("Authorization") accessToken: String,
         @Path("traceId") traceId: Int
     ): Call<Unit>
 }
