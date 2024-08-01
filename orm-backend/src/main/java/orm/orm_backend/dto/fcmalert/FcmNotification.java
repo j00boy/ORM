8c526085ef21addd192d816ec411895f82298d95
junbeom-Son <junbeom247@gmail.com>
2024-08-01 16:30:37 +0900

    fix : add getter method to fix on serializable

--- orm-backend/src/main/java/orm/orm_backend/dto/fcmalert/FcmNotification.java
+++ orm-backend/src/main/java/orm/orm_backend/dto/fcmalert/FcmNotification.java
@@ -1,8 +1,10 @@
 package orm.orm_backend.dto.fcmalert;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class FcmNotification {
 
     private final String title = "오름";
