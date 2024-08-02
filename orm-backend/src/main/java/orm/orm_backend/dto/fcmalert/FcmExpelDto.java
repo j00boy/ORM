fb4ee06ad5ee6e9b2ba7885f3f2bdac881f0451a
junbeom-Son <junbeom247@gmail.com>
2024-08-02 09:37:52 +0900

    fix : add final field value

--- orm-backend/src/main/java/orm/orm_backend/dto/fcmalert/FcmExpelDto.java
+++ orm-backend/src/main/java/orm/orm_backend/dto/fcmalert/FcmExpelDto.java
@@ -9,7 +9,7 @@ public class FcmExpelDto implements FcmAlertData {
     private String clubId;
     private String clubName;
     private String clubImageSrc;
-    private final AlertType alertType;
+    private final AlertType alertType = AlertType.EXPEL;
 
     @Override
     public String getMessage() {
