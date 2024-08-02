27540129d4d9ab4e058dabf0163975498b8746e7
junbeom-Son <junbeom247@gmail.com>
2024-08-02 10:11:10 +0900

    test : push alert on club application

--- orm-backend/src/test/java/orm/orm_backend/service/FirebaseServiceTest.java
+++ orm-backend/src/test/java/orm/orm_backend/service/FirebaseServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
@@ -14,6 +15,9 @@ import org.springframework.test.util.ReflectionTestUtils;
 import orm.orm_backend.entity.Club;
 import orm.orm_backend.entity.User;
 import orm.orm_backend.util.FirebaseUtil;
+
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
@@ -44,9 +48,26 @@ public class FirebaseServiceTest {
     @Mock
     Club club;
 
+    Integer userId = 6;
+    Integer clubId = 1;
+    String userName = "김세진";
+    String clubName = "백승헌 클럽";
+
+
     @BeforeEach
     void init() {
         ReflectionTestUtils.setField(firebaseUtil, "firebaseApiUrl", firebaseApiUrl);
         ReflectionTestUtils.setField(firebaseUtil, "firebaseConfigPath", firebaseConfigPath);
+
+        lenient().when(user.getId()).thenReturn(userId);
+        lenient().when(club.getId()).thenReturn(clubId);
+        lenient().when(user.getNickname()).thenReturn(userName);
+        lenient().when(club.getClubName()).thenReturn(clubName);
+        lenient().when(club.getImageSrc()).thenReturn(testImageSrc);
+    }
+
+    @Test
+    void pushClubApplicationAlertTest() {
+        firebasePushAlertService.pushClubApplicationAlert(testFirebaseToken, user, club);
     }
 }
