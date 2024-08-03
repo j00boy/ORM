1b593004338ffae0966afb6d6b9669609665344e
김세진 <991118ksj@naver.com>
2024-08-03 20:18:41 +0900

    fix : rollback database

--- ORM_Android/app/src/main/java/com/orm/data/local/AppDatabase.kt
+++ ORM_Android/app/src/main/java/com/orm/data/local/AppDatabase.kt
@@ -7,18 +7,16 @@ import com.orm.data.local.dao.ClubDao
 import com.orm.data.local.dao.MemberDao
 import com.orm.data.local.dao.NotificationDao
 import com.orm.data.local.dao.PointDao
-import com.orm.data.local.dao.RecordDao
 import com.orm.data.local.dao.TraceDao
 import com.orm.data.local.dao.TrailDao
 import com.orm.data.model.club.Club
 import com.orm.data.model.Member
 import com.orm.data.model.Notification
 import com.orm.data.model.Point
-import com.orm.data.model.Record
 import com.orm.data.model.Trace
 import com.orm.data.model.Trail
 
-@Database(entities = [Club::class, Trace::class, Member::class, Notification::class, Point::class, Trail::class, Record::class], version = 1, exportSchema = false)
+@Database(entities = [Club::class, Trace::class, Member::class, Notification::class, Point::class, Trail::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun clubDao(): ClubDao
@@ -27,5 +25,4 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun notificationDao(): NotificationDao
     abstract fun pointDao(): PointDao
     abstract fun trailDao(): TrailDao
-    abstract fun recordDao(): RecordDao
 }
