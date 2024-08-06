c6a012a537493bc3e9668a6f11d1909297a2c8eb
kkjjhh06 <kkjjhh06@naver.com>
2024-08-06 18:01:36 +0900

    feat: create CookieUtil

---
+++ orm-backend/src/main/java/orm/orm_backend/util/CookieUtil.java
@@ -0,0 +1,35 @@
+package orm.orm_backend.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Arrays;
+
+public class CookieUtil {
+
+    /**
+     * boardId에 해당하는 쿠키가 유효한지 확인
+     * @param boardId
+     * @param cookies
+     * @return
+     */
+    public static boolean checkCookie(String cookiePrefix, Integer boardId, Cookie[] cookies) {
+        String cookieName = cookiePrefix + "-" + boardId;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookieName.equals(cookie.getName())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void setCookie(String cookiePrefix, HttpServletResponse response, Integer boardId) {
+        Cookie cookie = new Cookie(cookiePrefix + "-" + boardId, "true");
+        cookie.setMaxAge(60);
+        cookie.setPath("/clubs/boards");
+        response.addCookie(cookie);
+    }
+}
