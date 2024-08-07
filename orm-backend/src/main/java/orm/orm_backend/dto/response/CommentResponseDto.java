0bef0eec010a8afadd35a36e568828b94df4ddfc
kkjjhh06 <kkjjhh06@naver.com>
2024-08-07 10:52:41 +0900

    feat: create createComment method

--- orm-backend/src/main/java/orm/orm_backend/dto/response/CommentResponseDto.java
+++ orm-backend/src/main/java/orm/orm_backend/dto/response/CommentResponseDto.java
@@ -4,14 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import orm.orm_backend.entity.Comment;
 import orm.orm_backend.entity.User;
-import orm.orm_backend.service.CommentService;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class CommentResponseDto {
-    private Integer commentId;
-    private Integer userId;
     private String userNickname;
     private String content;
     private LocalDateTime createdAt;
@@ -19,8 +16,6 @@ public class CommentResponseDto {
     @Builder
     public CommentResponseDto(Comment comment) {
         User user = comment.getUser();
-        this.commentId = comment.getId();
-        this.userId = user.getId();
         this.userNickname = user.getNickname();
         this.content = comment.getContent();
         this.createdAt = comment.getCreatedAt();
